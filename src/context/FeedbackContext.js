5198c8ca8f8938b050abd238c358a64fbd6a4a09
Nirbhay Lourembam <32927418+nirbhay-codes@users.noreply.github.com>
2024-03-24 14:43:24 +0530

    Add feedback via API

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -18,8 +17,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(`http://localhost:5000/feedback?_sort=-id`) // _order=desc is obsolete now
-
+    const response = await fetch(`/feedback?_sort=-id`) // _order=desc is obsolete now
     const data = await response.json()
 
     setFeedback(data)
@@ -27,9 +25,17 @@ export const FeedbackProvider = ({ children }) => {
   }
 
   // Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+    setFeedback([data, ...feedback])
   }
 
   // Delete feedback
