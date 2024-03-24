9d88eb62172eae3d18143505920e88ebe8b0b019
Nirbhay Lourembam <32927418+nirbhay-codes@users.noreply.github.com>
2024-03-24 16:55:53 +0530

    Bugfix: default rating of 10 does not show on load

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,17 +1,8 @@
 import { useContext, useEffect, useState } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
 
-function RatingSelect({ select }) {
-  const [selected, setSelected] = useState(10)
-
-  const { feedbackEdit } = useContext(FeedbackContext)
-
-  useEffect(() => {
-    setSelected(feedbackEdit.item.rating)
-  }, [feedbackEdit])
-
+function RatingSelect({ select, selected }) {
   const handleChange = (e) => {
-    setSelected(+e.currentTarget.value)
     select(+e.currentTarget.value)
   }
 
