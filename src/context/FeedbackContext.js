2a2a38728a039162afb946af057ae76adc477382
Nirbhay Lourembam <32927418+nirbhay-codes@users.noreply.github.com>
2024-03-24 15:47:33 +0530

    no need to spread data and item in updateFeedback as its not required with json-server API

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -67,7 +67,7 @@ export const FeedbackProvider = ({ children }) => {
     const data = await response.json()
 
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
+      feedback.map((item) => (item.id === id ? data : item))
     )
   }
 
